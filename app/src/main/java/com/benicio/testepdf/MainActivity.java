33019e44f270787fc248982cecf42287352d2b03
benicio <gustavo024dias@gmail.com>
2023-09-25 15:34:50 -0300

    falta ajustar componentes

--- app/src/main/java/com/benicio/testepdf/MainActivity.java
+++ app/src/main/java/com/benicio/testepdf/MainActivity.java
@@ -24,6 +24,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Type;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     // creating a bitmap variable
     // for storing our images
     Bitmap bmp, scaledbmp;
+    Bitmap bmp2, scaledbmp2;
     // constant code for runtime permissions
     private static final int PERMISSION_REQUEST_CODE = 200;
 
@@ -47,8 +49,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         botao = findViewById(R.id.botao);
+
         bmp = BitmapFactory.decodeResource(getResources(), R.drawable.brasao);
         scaledbmp = Bitmap.createScaledBitmap(bmp, 140, 140, false);
+
+        bmp2 = BitmapFactory.decodeResource(getResources(), R.drawable.brasao);
+        scaledbmp2 = Bitmap.createScaledBitmap(bmp, 140, 140, false);
 
         // below code is used for
         // checking our permissions.
@@ -98,75 +104,40 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void generatePDF() {
-        // creating an object variable
-        // for our PDF document.
         PdfDocument pdfDocument = new PdfDocument();
 
-        // two variables for paint "paint" is used
-        // for drawing shapes and we will use "title"
-        // for adding text in our PDF file.
         Paint paint = new Paint();
         Paint title = new Paint();
 
-        // we are adding page info to our PDF file
-        // in which we will be passing our pageWidth,
-        // pageHeight and number of pages and after that
-        // we are calling it to create our PDF.
         PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(pagewidth, pageHeight, 1).create();
-
-        // below line is used for setting
-        // start page for our PDF file.
         PdfDocument.Page myPage = pdfDocument.startPage(mypageInfo);
 
-        // creating a variable for canvas
-        // from our page of PDF.
         Canvas canvas = myPage.getCanvas();
-
-        // below line is used to draw our image on our PDF file.
-        // the first parameter of our drawbitmap method is
-        // our bitmap
-        // second parameter is position from left
-        // third parameter is position from top and last
-        // one is our variable for paint.
         canvas.drawBitmap(scaledbmp, 56, 40, paint);
+        canvas.drawBitmap(scaledbmp2, 580, 40, paint);
 
-        // below line is used for adding typeface for
-        // our text which we will be adding in our PDF file.
-        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
 
-        // below line is used for setting text size
-        // which we will be displaying in our PDF file.
-        title.setTextSize(15);
-
-        // below line is sued for setting color
-        // of our text inside our PDF file.
+        title.setTextSize(25);
+        title.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
         title.setColor(ContextCompat.getColor(this, R.color.black));
 
-        // below line is used to draw text in our PDF file.
-        // the first parameter is our text, second parameter
-        // is position from start, third parameter is position from top
-        // and then we are passing our variable of paint which is title.
-        canvas.drawText("A portal for IT professionals.", 209, 100, title);
-        canvas.drawText("Geeks for Geeks", 209, 80, title);
+        canvas.drawText("Relatório do projeto:", 300, 110, title);
+        canvas.drawText("XXXXXXXXXXX", 300, 140, title);
 
-        // similarly we are creating another text and in this
-        // we are aligning this text to center of our PDF file.
         title.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
         title.setColor(ContextCompat.getColor(this, R.color.black));
         title.setTextSize(15);
-
-        // below line is used for setting
-        // our text to center of PDF.
         title.setTextAlign(Paint.Align.CENTER);
         canvas.drawText("This is sample document which we have created.", 396, 560, title);
-
-        // after adding all attributes to our
-        // PDF file we will be finishing our page.
         pdfDocument.finishPage(myPage);
 
-        // below line is used to set the name of
-        // our PDF file and its path.
-        File file = new File(Environment.getExternalStorageDirectory(), "GFG.pdf");
+        File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        if (!directory.exists()) {
+            directory.mkdirs(); // Crie o diretório se ele não existir.
+        }
+
+        File file = new File(directory, "TESTEGFG.pdf");
 
         try {
             // after creating a file name we will
